a06aa1c972114dda504b82ecf3167aab43ad0a7e
yolomc <yolo.mc@icloud.com>
2020-08-03 20:17:11 +0800

    200803

---
+++ 101.对称二叉树.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=golang
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	return checkNode(root.Left, root.Right)
+}
+
+func checkNode(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && checkNode(p.Left, q.Right) && checkNode(p.Right, q.Left)
+}
+
+// @lc code=end
