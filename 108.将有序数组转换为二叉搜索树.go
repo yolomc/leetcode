c02b041b26ecb82cddb3f8d8b93b01944e21716e
yolomc <yolo.mc@icloud.com>
2020-08-06 23:44:05 +0800

    200806

---
+++ 108.将有序数组转换为二叉搜索树.go
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=golang
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	mid := len(nums) / 2
+	tree := &TreeNode{Val: nums[mid]}
+	tree.Left = sortedArrayToBST(nums[:mid])
+	tree.Right = sortedArrayToBST(nums[mid+1:])
+
+	return tree
+}
+
+// @lc code=end
